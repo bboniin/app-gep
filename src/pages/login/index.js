fd465aae3f8741a63426e217d4c0c1d5d8b03908
bboniin <boninho7834@gmail.com>
2020-07-29 07:44:51 -0300

    correção login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,135 +1,181 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-    View,
-    Image,
-    TextInput,
-    AsyncStorage,
-    ImageBackground,
-    TouchableOpacity,
-    Alert
+  View,
+  Image,
+  TextInput,
+  AsyncStorage,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
 } from 'react-native';
 
 import firebase from './../../services/firebase';
 
-import {
-    Button,
-    Dialog,
-    Paragraph,
-    Portal,
-    Text
-} from 'react-native-paper';
+import {Button, Dialog, Paragraph, Portal, Text} from 'react-native-paper';
 import fundo from './../../images/fundo.png';
 
-import logo from './../../images/logo.png'
-import boneco from './../../images/boneco.png'
-import { Botao } from './styles'
+import logo from './../../images/logo.png';
+import boneco from './../../images/boneco.png';
+import {Botao} from './styles';
 
-export default class Login extends Component{
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      senha: '',
+      usuarios: [],
+      visible: false,
+      error: true,
+    };
+  }
 
-    constructor(props){
-        super(props);
-            this.state ={
-                user: '',
-                senha: '',
-                usuarios: [],
-                visible: false,
-                error: true 
-            }
-    }
+  async handlerStorage(item) {
+    await AsyncStorage.setItem('usuario', JSON.stringify(item));
+  }
 
-    async handlerStorage(item){
-        await AsyncStorage.setItem('id', `${item.id}`);
-        await AsyncStorage.setItem('usuario', JSON.stringify(item))
-     }
+  async componentDidMount() {
+    await AsyncStorage.removeItem('usuario');
+    await firebase
+      .database()
+      .ref('usuarios')
+      .on('value', (item) => {
+        let dados = this.state;
+        item.forEach((data) => {
+          dados.usuarios.push({
+            metafinal: data.val().metafinal,
+            usuario: data.val().usuario,
+            tipo: data.val().tipo,
+            senha: data.val().senha,
+            nome: data.val().nome,
+            foto: data.val().foto,
+            nomedeurna: data.val().nomedeurna,
+            numero: data.val().numero,
+            partido: data.val().partido,
+            telefone: data.val().telefone,
+            endereco: data.val().endereco,
+            progresso: data.val().progresso,
+          });
+        });
+        this.setState(dados);
+      });
+  }
 
-    async  componentDidMount(){
-      await   firebase.database().ref('usuarios').on('value', (item)=>{
-            let dados = this.state;
-            item.forEach((data)=>{   
-                dados.usuarios.push({
-                    metafinal: data.val().metafinal,
-                    usuario: data.val().usuario,
-                    tipo: data.val().tipo,
-                    senha: data.val().senha,
-                    nome: data.val().nome,
-                    foto: data.val().foto,
-                    nomedeurna: data.val().nomedeurna,
-                    numero: data.val().numero,
-                    partido: data.val().partide,
-                    telefone: data.val().telefone,
-                    endereco: data.val().endereco,
-                    progresso: data.val().progresso
-                })
-            })
-            this.setState(dados);
-        })
-    }
+  render() {
+    return (
+      <ImageBackground source={fundo} style={{flex: 1}}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Image
+            source={logo}
+            style={{width: 1000, height: 150, resizeMode: 'contain'}}
+          />
+          <Image
+            source={boneco}
+            style={{width: 1000, height: 150, resizeMode: 'contain'}}
+          />
 
-    render(){
-        return(
-            <ImageBackground source={fundo} style={{flex: 1}}>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Image source={logo}  style={{width: 1000, height: 150, resizeMode: 'contain'}}/>
-                <Image source={boneco}  style={{width: 1000, height: 150, resizeMode: 'contain'}}/>
-
-                <TextInput
-                value={this.state.user}
-                onChangeText={ (text) => {this.setState({user: text})}}
-                placeholder="Usuario"
-                style={{width: '75%', borderRadius: 5, marginTop: 25, paddingLeft: 12, fontSize: 16, backgroundColor: 'white'}}
-                >
-                </TextInput>
-                <TextInput
-                value={this.state.senha}
-                secureTextEntry={true}
-                onChangeText={ (text) => {this.setState({senha: text})}}
-                placeholder="Senha"
-                style={{width: '75%', borderRadius: 5, marginTop: 25, paddingLeft: 12, fontSize: 16, backgroundColor: 'white'}}
-                >
-                </TextInput>
-                <Botao
-                    onPress={()=>{
-                        let dados = this.state
-                        for(let i = 0; i<=(dados.usuarios.length-1); i++){
-                            if(this.state.user == dados.usuarios[i].usuario  && this.state.senha == dados.usuarios[i].senha ){
-                                dados.error = false,
-                                this.setState({dados})
-                                this.handlerStorage(dados.usuarios[i]);
-                                this.props.navigation.navigate('Home');
-                            }
-                        } 
-                            if(dados.error == true){
-                                Alert.alert("Error","Usuário ou Senha Inválidos")
-                            }
-                    
-                    }}
-                >
-                    <Text style={{fontSize: 16, fontWeight: 'bold', fontStyle: 'normal', color: 'black'}}>ENTRAR</Text>
-                </Botao>
-                <TouchableOpacity style={{marginTop: 20, opacity: 0.5, color: '#FFF'}}
-                onPress={()=>{this.setState({visible: true})}}>
-                    <Text style={{color: '#FFF'}}>ESQUECI A SENHA</Text>
+          <TextInput
+            value={this.state.user}
+            onChangeText={(text) => {
+              this.setState({user: text});
+            }}
+            placeholder="Usuario"
+            style={{
+              width: '75%',
+              borderRadius: 5,
+              marginTop: 25,
+              paddingLeft: 12,
+              fontSize: 16,
+              backgroundColor: 'white',
+            }}></TextInput>
+          <TextInput
+            value={this.state.senha}
+            secureTextEntry={true}
+            onChangeText={(text) => {
+              this.setState({senha: text});
+            }}
+            placeholder="Senha"
+            style={{
+              width: '75%',
+              borderRadius: 5,
+              marginTop: 25,
+              paddingLeft: 12,
+              fontSize: 16,
+              backgroundColor: 'white',
+            }}></TextInput>
+          <Botao
+            onPress={() => {
+              let dados = this.state;
+              for (let i = 0; i <= dados.usuarios.length - 1; i++) {
+                if (
+                  this.state.user == dados.usuarios[i].usuario &&
+                  this.state.senha == dados.usuarios[i].senha
+                ) {
+                  dados.error = false;
+                  let user = dados.usuarios[i];
+                  this.handlerStorage(user);
+                  this.props.navigation.navigate('Home');
+                }
+              }
+              if (dados.error == true) {
+                Alert.alert('Error', 'Usuário ou Senha Inválidos');
+              }
+            }}>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: 'bold',
+                fontStyle: 'normal',
+                color: 'black',
+              }}>
+              ENTRAR
+            </Text>
+          </Botao>
+          <TouchableOpacity
+            style={{marginTop: 20, opacity: 0.5, color: '#FFF'}}
+            onPress={() => {
+              this.setState({visible: true});
+            }}>
+            <Text style={{color: '#FFF'}}>ESQUECI A SENHA</Text>
+          </TouchableOpacity>
+          <Portal>
+            <Dialog visible={this.state.visible}>
+              <Dialog.Content>
+                <Paragraph>
+                  {'Solicite sua senha no número\n(24) 99856-5959'}
+                </Paragraph>
+              </Dialog.Content>
+              <Dialog.Actions>
+                <TouchableOpacity
+                  color="black"
+                  labelStyle={{
+                    borderWidth: 1,
+                    padding: 4,
+                    borderRadius: 5,
+                    color: 'black',
+                    fontSize: 16,
+                    fontWeight: 'bold',
+                  }}
+                  onPress={() => {
+                    let dados = this.state;
+                    dados.visible = false;
+                    this.setState({dados});
+                  }}>
+                  <Text
+                    style={{
+                      color: '#ffcc00',
+                      fontSize: 16,
+                      margin: 15,
+                      fontWeight: 'bold',
+                    }}>
+                    FECHAR
+                  </Text>
                 </TouchableOpacity>
-                <Portal>
-                    <Dialog
-                    visible={this.state.visible}>
-                    <Dialog.Content>
-                    <Paragraph>{"Solicite sua senha no número\n(24) 99856-5959"}</Paragraph>
-                    </Dialog.Content>
-                    <Dialog.Actions>
-                        <TouchableOpacity
-                        color="black"
-                        labelStyle={{borderWidth: 1, padding: 4, borderRadius: 5, color: 'black', fontSize: 16, fontWeight: 'bold' }}
-                        onPress={() =>{
-                         let dados = this.state
-                        dados.visible = false
-                         this.setState({dados})
-                        }}><Text style={{color: '#ffcc00', fontSize: 16,  margin: 15,fontWeight: 'bold'}}>FECHAR</Text></TouchableOpacity>
-                    </Dialog.Actions>
-                    </Dialog>
-                </Portal>
-            </View>
-            </ImageBackground>
-        );
-    }
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
+        </View>
+      </ImageBackground>
+    );
+  }
 }
